5cc5463853c1f630bbed768ecb76fb3797ae8a94
NOUNI El Bachir <nouni.elbachir@gmail.com>
2019-01-10 23:07:39 +0100

    + Install laravel ide helper + localize auth views text

---
+++ resources/lang/en/text.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'app_name' => 'Q-Check',
+    'features' => [
+        'agents' => 'Agents de contrôle',
+        'unites' => 'Unité de contrôle',
+        'fiches' => 'Fiches de contrôle',
+        'statistiques' => 'Statistiques',
+    ],
+    'home-link' => 'Acceuil',
+    'login-link' => 'Connexion',
+    'logout-link' => 'Quitter',
+    'register-link' => 'Inscription',
+    'welcome-page-title' => 'Q-Check Web App',
+    'login-form' => [
+        'title' => 'Connexion',
+        'email' => 'Addresse Email',
+        'pwd' => 'Mot de passe',
+        'remember-me' => 'Se rappler de moi',
+        'login-btn' => 'Se connecter',
+        'pwd-forget-link' => 'Mot de passe oublié'
+    ],
+    'register-form' => [
+        'title' => 'Inscription',
+        'name' => 'Nom',
+        'email' => 'Addresse Email',
+        'pwd' => 'Mot de passe',
+        'pwd-confirm' => 'Confirmation Mot de passe',
+        'register-btn' => "S'inscrir",
+    ],
+    'home'=>[
+        'title'=>'Acceuil',
+    ]
+];
